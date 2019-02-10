d9ff33e520f4e74442857f46c6bc50dd451ad314
Christopher Wang <christopher.jj.wang@gmail.com>
2019-02-10 07:56:03 -0500

    Addeda config file

--- Game/swervin_mervin/main.py
+++ Game/swervin_mervin/main.py
@@ -5,7 +5,6 @@
 import pygame
 import game as g
 import settings as s
-import yolov3 as y
 
 pygame.init()
 
@@ -19,8 +18,7 @@ else:
 
 fps_clock = pygame.time.Clock()
 window    = pygame.display.set_mode(s.DIMENSIONS, w_flag)
-yolov3 = y.yolov3(s.DIMENSIONS)
-game      = g.Game(yolov3, window, fps_clock)
+game      = g.Game(window, fps_clock)
 
 while True:
    if game.waiting:
